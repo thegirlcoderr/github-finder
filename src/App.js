cf20cd9207b1b40f6b614aee62420db712c2af7e
thegirlcoderr <lynndy2015@gmail.com>
2023-08-23 18:41:28 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1 className="mt-5 py-6 text-center text-3xl font-bold ">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
+
+
+export default App
